6d784c87c5df4800995997d905dda41ef3d69028
l Iz <n0v0_dvlpmt@outlook.com>
2026-01-06 16:01:01 -0600

    adding user session history - part 1

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -6,6 +6,7 @@ import {vapi} from "@/lib/vapi.sdk";
 import Image from "next/image";
 import Lottie, {LottieRefCurrentProps, useLottie} from "lottie-react";
 import soundwaves from '@/constants/soundwaves.json';
+import {addToSessionHistory} from "@/lib/actions/companion.actions";
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -38,7 +39,10 @@ const CompanionComponent = ({ companionId, subject, topic, name,
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
 
-        const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+        const onCallEnd = () => {
+            setCallStatus(CallStatus.FINISHED);
+            addToSessionHistory(companionId)
+        }
 
         const onMessage = (message: Message) =>{
             if(message.type === 'transcript' && message.transcriptType === 'final' ) {
